f379869db4d639c62c0859e254c090b01a204ac0
alexfy <alexfy@bu.edu>
2023-05-29 15:17:32 -0400

    Completed Chapter 2, and added detailed comments

--- ch02/ch02.py
+++ ch02/ch02.py
@@ -5,6 +5,69 @@ Created on Wed May 24 20:53:42 2023
 @author: alexfy1015
 """
 
+# *Python Machine Learning 2nd Edition* by [Sebastian Raschka](https://sebastianraschka.com), Packt Publishing Ltd. 2017
+# 
+# Code Repository: https://github.com/rasbt/python-machine-learning-book-2nd-edition
+# 
+# Code License: [MIT License](https://github.com/rasbt/python-machine-learning-book-2nd-edition/blob/master/LICENSE.txt)
+
+# # Python Machine Learning - Code Examples
+
+# # Chapter 2 - Training Machine Learning Algorithms for Classification
+
+# Note that the optional watermark extension is a small IPython notebook plugin that I developed to make the code reproducible. You can just skip the following line(s).
+
+
+
+
+
+# *The use of `watermark` is optional. You can install this IPython extension via "`pip install watermark`". For more information, please see: https://github.com/rasbt/watermark.*
+
+# ### Overview
+# 
+
+# - [Artificial neurons – a brief glimpse into the early history of machine learning](#Artificial-neurons-a-brief-glimpse-into-the-early-history-of-machine-learning)
+#     - [The formal definition of an artificial neuron](#The-formal-definition-of-an-artificial-neuron)
+#     - [The perceptron learning rule](#The-perceptron-learning-rule)
+# - [Implementing a perceptron learning algorithm in Python](#Implementing-a-perceptron-learning-algorithm-in-Python)
+#     - [An object-oriented perceptron API](#An-object-oriented-perceptron-API)
+#     - [Training a perceptron model on the Iris dataset](#Training-a-perceptron-model-on-the-Iris-dataset)
+# - [Adaptive linear neurons and the convergence of learning](#Adaptive-linear-neurons-and-the-convergence-of-learning)
+#     - [Minimizing cost functions with gradient descent](#Minimizing-cost-functions-with-gradient-descent)
+#     - [Implementing an Adaptive Linear Neuron in Python](#Implementing-an-Adaptive-Linear-Neuron-in-Python)
+#     - [Improving gradient descent through feature scaling](#Improving-gradient-descent-through-feature-scaling)
+#     - [Large scale machine learning and stochastic gradient descent](#Large-scale-machine-learning-and-stochastic-gradient-descent)
+# - [Summary](#Summary)
+
+#%%
+
+# # Artificial neurons - a brief glimpse into the early history of machine learning
+
+
+
+
+
+# ## The formal definition of an artificial neuron
+
+
+
+
+
+# ## The perceptron learning rule
+
+
+
+
+
+
+
+
+
+
+# # Implementing a perceptron learning algorithm in Python
+
+# ## An object-oriented perceptron API
+
 import numpy as np
 
 class Perceptron(object):
@@ -164,6 +227,23 @@ plt.show()
 
 #%%
 
+# # Adaptive linear neurons and the convergence of learning
+
+# ...
+
+# ## Minimizing cost functions with gradient descent
+
+
+
+
+
+
+
+
+
+
+# ## Implementing an adaptive linear neuron in Python
+
 class AdalineGD(object):
     """ADAptaive LInear NEuron classifier.
     
@@ -212,6 +292,13 @@ class AdalineGD(object):
         
         for _ in range(self.n_iter):
             net_input = self.net_input(X)
+            # Please note that the "activation" method has no effect
+            # in the code since it is simply an identity function. We
+            # could write `output = self.net_input(X)` directly instead.
+            # The purpose of the activation is more conceptual, i.e.,  
+            # in the case of logistic regression (as we will see later), 
+            # we could change it to
+            # a sigmoid function to implement a logistic regression classifier.
             output = self.activation(net_input)
             errors = (y - output)
             self.w_[1:] += self.eta * X.T.dot(errors)
@@ -250,4 +337,162 @@ ax[1].set_xlabel('Epochs')
 ax[1].set_ylabel('log(Sum-squared-error')
 ax[1].set_title('Adaline - Learning rate 0.0001')
 plt.show()
+
+#%%
+
+# ## Improving gradient descent through feature scaling
+
+
+
+
+
+
+
+# standardize features
+
+X_std = np.copy(X)
+X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()   
+X_std[:,1] = (X_std[:,1] - X_std[:,1].mean()) / X_std[:,1].std()
+
+#%%
+
+ada = AdalineGD(n_iter=15, eta=0.01)
+ada.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
+plt.tight_layout()
+plt.show()
+
+#%%
+
+# ## Large scale machine learning and stochastic gradient descent
+
+class AdalineSGD(object):
+    """ADAptaive LInear NEuron classifier.
     
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    random_state : int
+      Random number generator seed for random weight initialization.
+      
+    Attributes
+    ------------
+    w_ : 1d-array
+      Weights after fitting.
+    cost_ : list
+      Sum-of-squares cost function value averaged over all 
+      training samples in each epoch.
+    
+    """
+    
+    def __init__(self, eta=0.01, n_iter =10,
+                 shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        self.random_state = random_state
+        
+    def fit(self, X, y):
+        """Fit training data.
+        
+        Parameters
+        ------------
+        X : {array-like}, shape = [n_samples, n_features]
+          Training vectors, where n_samples is the number of samples and
+          n_features is the number of features.
+        y : array-like, shape = [n_samples]
+          Target values.
+          
+        Returns
+        ------------
+        self : object
+        
+        """
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost) / len(y)
+            self.cost_.append(avg_cost)
+        return self
+    
+    def partial_fit(self, X, y):
+        """Fit training data without reinitializing the weights"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip (X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+    
+    def _shuffle(self, X, y):
+        """Shuffle training data"""
+        r = self.rgen.permutation(len(y))
+        return X[r], y[r]
+    
+    def _initialize_weights(self, m):
+        """Initialize weights to small random numbers"""
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc=0.0, scale=0.01, 
+                                   size=1 + m)
+        self.w_initialized = True
+    
+    def _update_weights(self, xi, target):
+        """Apply Adaline learning rule to update the weights"""
+        output = self.activation(self.net_input(xi))
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error**2
+        return cost
+    
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    def activation(self, X):
+        """Compute linear activation"""
+        return X
+    
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
+    
+#%%
+
+ada = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+ada.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Stochastic Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Average Cost')
+plt.tight_layout()
+plt.show()
