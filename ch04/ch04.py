93445d85821dfd2e82ea2a9e1b81f0fb42556169
alexfy1015 <tidus1015@gmail.com>
2023-06-26 22:55:24 -0400

    partitioning datasets into train/test sets

--- ch04/ch04.py
+++ ch04/ch04.py
@@ -99,3 +99,45 @@ pd.get_dummies(df[['price', 'color', 'size']],
 
 ohe.fit_transform(X)[:,1:] #This call can drop the first column in the previous ohe, and hence remove multicollinearity
 
+#%%
+df_wine = pd.read_csv('https://archive.ics.uci.edu/'
+                      'ml/machine-learning-databases/'
+                      'wine/wine.data', header=None)
+df_wine.columns=['Class label', 'Alcohol',
+                 'Malic acid', 'Ash',
+                 'Alcalinity of ash', 'Magnesium',
+                 'Total phenols', 'Flavanoids',
+                 'Nonflavanoid phenols',
+                 'Proanthocyanins',
+                 'Color intensity', 'Hue',
+                 'OD280/OD315 of diluted wines',
+                 'Proline']
+print('Class labels', np.unique(df_wine['Class label']))
+df_wine.head()
+
+#%%
+from sklearn.model_selection import train_test_split
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test =\
+    train_test_split(X, y,
+                     test_size=0.3,
+                     random_state=0,
+                     stratify=y)
+
+#%%
+from sklearn.preprocessing import MinMaxScaler
+mns = MinMaxScaler()
+X_train_norm = mns.fit_transform(X_train)
+X_test_norm = mns.transform(X_test)
+
+#%%
+ex = np.array([0, 1, 2, 3, 4, 5])
+print('standardized:', (ex - ex.mean()) / ex.std())
+print('normalized:', (ex - ex.min()) / (ex.max() - ex.min()))
+
+#%%
+from sklearn.preprocessing import StandardScaler
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
+
